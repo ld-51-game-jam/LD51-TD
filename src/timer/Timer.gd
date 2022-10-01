6db6719bbbedf11a8a3cac05915a8e3206d7a234
brntcllns <floyd1911@gmail.com>
2022-09-30 22:12:00 -0700

    Add return types to timer functions

--- src/timer/Timer.gd
+++ src/timer/Timer.gd
@@ -1,10 +1,10 @@
 extends Timer
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void: 
 	pass # Replace with function body.
 
 
 # This method was just a proof of concept for testing the timeout from the time scene
-func _on_Timer_timeout():
+func _on_Timer_timeout() -> void:
 	print("10 second timeout")
