e9369e4ecdbe26b74765b1738abf9555e05692b5
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-10-01 14:45:19 -0700

    some small mechanics for the enemy animation

--- src/enemies/Enemy.gd
+++ src/enemies/Enemy.gd
@@ -1,9 +1,42 @@
 extends "res://src/enemies/Base.gd"
 
+var timer_started: bool = false
+
+# toggle on or off from inspector
+export var _demo_mode = false
+#for demo purposes only.
+var demo_walk_count: int = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.start() # we would call this method once the player is "killed"
-
+	$AnimatedSprite.animation = "human_walking_south"
+	$AnimatedSprite.playing = true
+	
 
 func _on_Timer_timeout() -> void:
-	print("timeout for human")
+	$AnimatedSprite.animation = "revive"
+	timer_started = false
+	
+	
+func _process(delta: float) -> void:
+	if $AnimatedSprite.animation == "dead" and timer_started == false:
+		$Timer.start()
+		timer_started = true
+
+
+func _on_AnimatedSprite_animation_finished() -> void:
+	if $AnimatedSprite.animation == "death":
+		$AnimatedSprite.animation = "dead"
+		
+	if $AnimatedSprite.animation == "human_walking_south":
+		if _demo_mode:
+			demo_walk_count = demo_walk_count + 1
+			if demo_walk_count > 3:
+				$AnimatedSprite.animation = "death"
+				demo_walk_count = 0
+			
+	if $AnimatedSprite.animation == "revive":
+		if _demo_mode:
+			$AnimatedSprite.animation = "human_walking_south"
+			
