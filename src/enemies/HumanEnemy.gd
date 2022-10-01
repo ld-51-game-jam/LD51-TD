047dcffa07fdb4a6943d0244b8377519a87d93b7
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-09-30 22:35:37 -0700

    Adding some boilerplate to the human enemies before zombies

---
+++ src/enemies/HumanEnemy.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.start() # we would call this method once the player is "killed"
+
+
+func _on_Timer_timeout() -> void:
+	print("timeout for human")
