e4561a5f8c7751504f81eb12b65034c63dc7102f
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-10-01 23:41:30 -0700

    update

--- src/start_menu/StartScene.gd
+++ src/start_menu/StartScene.gd
@@ -1,6 +1,6 @@
 extends EnemySpawner
 
-func _ready():
+func _ready() -> void:
 	wave_data = [["Enemy", 0.9],["Enemy", 0.8],["Enemy", 0.7],["Enemy", 0.6],["Enemy", 0.5],["Enemy", 0.4],["Enemy", 0.3],["Enemy", 0.2],["Enemy", 0.1]]
 
 func _on_QuitButton_pressed() -> void:
