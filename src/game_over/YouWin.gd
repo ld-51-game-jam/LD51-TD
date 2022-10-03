2f13d9b6645eb63589befc3dd05214fae00b26c9
sculpey <manda.tillman@gmail.com>
2022-10-02 23:27:22 -0500

    win

--- src/game_over/YouWin.gd
+++ src/game_over/YouWin.gd
@@ -5,7 +5,8 @@ extends Node2D
 # var a: int = 2
 # var b: String = "text"
 
-
+func _ready() -> void:
+	$AnimationPlayer.play("win")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
