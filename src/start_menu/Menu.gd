623a365d83d091314403e01558b6c2d95b265f2c
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-10-02 19:05:01 -0700

    Adding a price on each tower card

--- src/start_menu/Menu.gd
+++ src/start_menu/Menu.gd
@@ -1,21 +1,6 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_StartButton_pressed():
 	GameData.hp = 5
 	GameData.gold = 5
