fb0b51dbad33b3500c2d366211c063ebeccdf5c5
sculpey <manda.tillman@gmail.com>
2022-10-02 22:04:19 -0500

    game over text

--- src/game_over/GameOver.gd
+++ src/game_over/GameOver.gd
@@ -8,6 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$AnimationPlayer.play("drop")
 	pass # Replace with function body.
 
 func _on_QuitButton_pressed() -> void:
