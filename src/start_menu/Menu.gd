c8d251819d48a9fcf2227890c27e6475222b6d1f
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-10-02 19:06:22 -0700

    update

--- src/start_menu/Menu.gd
+++ src/start_menu/Menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
 
-func _on_StartButton_pressed():
+func _on_StartButton_pressed() -> void:
 	GameData.hp = 5
 	GameData.gold = 5
 	get_tree().change_scene("res://src/levels/LevelOne.tscn")
 
 
-func _on_OptionsButton_pressed():
-	pass # Replace with function body.
+func _on_OptionsButton_pressed() -> void:
+	pass # @todo - add options scene menu
 
